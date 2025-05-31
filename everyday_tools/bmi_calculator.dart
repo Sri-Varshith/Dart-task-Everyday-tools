10c470f4a1dd5c65db80918576688c2babf1106f
Sri Varshith <nvarshith.2007@gmail.com>
2025-05-31 17:58:18 +0530

    updated bmi added category

--- everyday_tools/bmi_calculator.dart
+++ everyday_tools/bmi_calculator.dart
@@ -5,5 +5,19 @@ void runBMICalculator(){
   double m = double.parse(stdin.readLineSync()!);
   print("Enter your height: ");
   double h = double.parse(stdin.readLineSync()!);
-  print("BMI is ${m/(h*h)}");
+  double bmi = m/(h*h);
+  String? str = null;
+  if(bmi<18.5){
+    str = "underweight";
+  }
+  else if(bmi<25){
+    str = "normal";
+  }
+  else if(bmi<30){
+    str = "overweight";
+  }
+  else{
+    str = "obese";
+  }
+  print("BMI is ${bmi} and you are ${str}");
 }
--- everyday_tools/main.dart
+++ everyday_tools/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'unit_converter.dart';
 import 'calculator.dart';
 import 'bmi_calculator.dart';
+import 'report_card.dart';
 void main() {
   while (true) {
     print('\n Everyday Tools');
@@ -17,7 +18,7 @@ void main() {
       case '1': runUnitConverter(); break;
       case '2': runCalculator(); break;
       case '3': runBMICalculator(); break;
-      // case '4': runReportCard(); break;
+      case '4': runReportCard(); break;
       case '5': exit(0);
       default: print('Invalid option');
     }
